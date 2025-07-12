660a4061a872be1e6cc7023543fbfc755b75b96b
julialingg <julialing0814@gmail.com>
2025-07-12 16:32:52 +0200

    build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-docker build -t a11y/angular-27742:latest -f dockerfiles/Dockerfile.27742  .
+docker build -t a11y/angular-26673:latest -f dockerfiles/Dockerfile.26673 .
