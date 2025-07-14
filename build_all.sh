d80b3fa5fb4f653e79ee7856836ad3923c3c87db
julialingg <julialing0814@gmail.com>
2025-07-14 10:53:57 +0200

    rebuild docker image

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+docker build -t a11y/angular-29793:latest -f dockerfiles/Dockerfile.29793 .
+docker build -t a11y/angular-27742:latest -f dockerfiles/Dockerfile.27742 .
+docker build -t a11y/angular-27317:latest -f dockerfiles/Dockerfile.27317 .
+docker build -t a11y/angular-27025:latest -f dockerfiles/Dockerfile.27025 .
 docker build -t a11y/angular-26673:latest -f dockerfiles/Dockerfile.26673 .
